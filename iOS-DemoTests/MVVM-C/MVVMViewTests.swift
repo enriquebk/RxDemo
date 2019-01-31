47a4c64224145ad68b0a5415cecdd8b849d65bb1
Enrique <ebermudez.ing@gmail.com>
2019-01-31 13:10:32 +0100

    Add MVVMView tests

---
+++ iOS-DemoTests/MVVM-C/MVVMViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  MVVMViewTests.swift
+//  iOS-DemoTests
+//
+//  Created by Enrique Bermúdez on 1/31/19.
+//  Copyright © 2019 Enrique Bermudez. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Demo
+
+class MVVMViewTests: XCTestCase {
+
+     let fakeViewModel = FakeViewModel()
+    
+    func testUIViewBinding() {
+       
+        let view = ViewMVVMViewMock()
+        
+        view.bind(to: fakeViewModel)
+        
+        XCTAssertTrue(view.didCallBindViewModel)
+        XCTAssertNotNil(view.viewModel)
+    }
+    
+    func testUIViewControllerBinding() {
+        
+        let viewController = ViewControllerMVVMViewMock()
+
+        viewController.bind(to: fakeViewModel)
+        
+        XCTAssertTrue(viewController.didCallBindViewModel)
+        XCTAssertTrue(viewController.didCallViewDidLoad)
+        XCTAssertNotNil(viewController.viewModel)
+    }
+}
