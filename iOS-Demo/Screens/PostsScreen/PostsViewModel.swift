8d7e7be9bfdcf3cc24decbfa7dc765fcb04a7b88
Enrique <ebermudez.ing@gmail.com>
2019-01-31 11:46:28 +0100

    Remove Main.storyboard

---
+++ iOS-Demo/Screens/PostsScreen/PostsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PostsViewModel.swift
+//  iOS-Demo
+//
+//  Created by Enrique Bermúdez on 1/31/19.
+//  Copyright © 2019 Enrique Bermudez. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewModel: ViewModel, CoordinatorManager {
+    
+    var coordinator: Coordinator<PostsRoute>!
+
+}
