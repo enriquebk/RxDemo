c69313f5e218966d2cfbf738d74af58b2592e4c5
Enrique <ebermudez.ing@gmail.com>
2019-02-04 08:13:08 +0100

    Implement PostDetailsViewController

--- iOS-Demo/Screens/PostDetailsScreen/PostDetailsViewController.swift
+++ iOS-Demo/Screens/PostDetailsScreen/PostDetailsViewController.swift
@@ -7,10 +7,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class PostDetailsViewController: UIViewController, MVVMView {
+   
+    @IBOutlet weak var postTitleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var comentsCountLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    
+    let disposeBag = DisposeBag()
     
     var viewModel: PostDetailsViewModel!
     
-    func bindViewModel() { }
+    func bindViewModel() {
+        
+        self.viewModel.post.subscribe { [weak self] event in
+            guard let post = event.element else { return }
+            self?.postTitleLabel.text = post.title
+            self?.bodyLabel.text = post.body
+        }.disposed(by: disposeBag)
+        
+        self.viewModel.commentsCount.subscribe { [weak self] event in
+            guard let commentCount = event.element else { return }
+            self?.comentsCountLabel.text = "\(commentCount)"
+        }.disposed(by: disposeBag)
+        
+        self.viewModel.author.subscribe { [weak self] event in
+            guard let author = event.element else { return }
+            self?.authorLabel.text = author.name
+            }.disposed(by: disposeBag)
+    }
 }
